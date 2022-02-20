35acc102e895f3628b75d043630cf4c021fd5b3c
kaijietti <guokaijie45@gmail.com>
2022-02-20 23:36:06 +0800

    update logstash config to parse log

--- observer/receiver/main.go
+++ observer/receiver/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type log struct {
+	Offset    int    `json:"offset"`
+	Message   string `json:"message"`
+	Node      string `json:"node"`
+	LogType   string `json:"log_type"`
+	Module    string `json:"module"`
+	Timestamp int    `json:"unix_timestamp"`
+}
+
 func newlog(w http.ResponseWriter, req *http.Request) {
 	fmt.Println(req.URL.Path)
 	body, _ := ioutil.ReadAll(req.Body)
-	fmt.Println(string(body))
+	log := log{}
+	if err := json.Unmarshal(body, &log); err != nil {
+		fmt.Println("ERROR")
+		return
+	}
+	fmt.Println(log)
 }
 
 func main() {
